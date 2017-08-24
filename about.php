1c98adfc586313fac4fd894f1c32d77a4ffca937
chrismomdjian <chrismomdjian@gmail.com>
2017-08-23 22:16:57 -0700

    added modals

--- about.php
+++ about.php
@@ -44,7 +44,7 @@
 	<div class="row text-center">
 		<h1 style="font-size: 3em;" class="headingTitle">Hobbies</h1>
 		<div class="col-xs-12 col-md-4">
-			<a href="#">
+			<a href="#" data-toggle="modal" data-target="#modal-1">
 				<img src="images/gameboy.png" alt="Flat UI Design Gameboy" class="icons img-responsive center-block img-circle">
 			</a>
 			<h2>Nintendo Nerd</h2>
@@ -56,12 +56,66 @@
 			<h2>Skateboarder</h2>
 		</div>
 		<div class="col-xs-12 col-md-4">
-			<a href="#">
+			<a href="#" data-toggle="modal" data-target="#modal-2">
 				<img src="images/camera.png" alt="Flat UI Design Camera" class="icons img-responsive center-block img-circle">
 			</a>
 			<h2>Photographer</h2>
 		</div>
 	</div>
+</div>
+
+<!--/////////////////// Modals /////////////////////////-->
+
+<!-- Nintendo Modal -->
+<div id="modal-1" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h2 class="modal-title text-center">I love retro video games!</h2>
+      </div>
+      <div class="modal-body">
+        <p>
+					I enjoy playing classic NES games like Contra and Super Mario from time to time.
+					I've owned many different consoles since I was little including the Super Nintendo,
+					Sega Genesis, Nintendo 64 and many others.
+				</p>
+
+				<p>
+					Whenever I get the chance I enjoy going to the closest retro video game store in my area and checking out the classic video game selections they offer.
+				</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<!-- Photography Modal -->
+<div id="modal-2" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h2 class="modal-title text-center">Street Photographer</h2>
+      </div>
+      <div class="modal-body">
+        <p>My brother gave me his old Canon AE-1 SLR camera when I was younger and I learned how to take decent looking pictures with it. Ever since he gave me that camera I had been interested in the different types of street moments that many artists capture when travelling to different areas around the world.</p>
+
+				<p>One day I would like to travel to Japan and South Korea equipped with a compact digital camera to take some cool street photos!</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
 </div>
 
 <?php include("inc/footer.php"); ?>
